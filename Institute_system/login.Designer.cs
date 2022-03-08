1174b4563691ee6441130287422707a36bc1eb76
Mohamed Elsayed <odsh97@gmail.com>
2022-03-08 22:41:34 +0200

    fixing Logo local path Bug

--- Institute_system/login.Designer.cs
+++ Institute_system/login.Designer.cs
@@ -35,16 +35,17 @@ namespace Institute_system
             this.userNameTextBox = new System.Windows.Forms.TextBox();
             this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.loginTypeComboBox = new System.Windows.Forms.ComboBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
             this.SuspendLayout();
             // 
             // logInBtn
             // 
-            this.logInBtn.Location = new System.Drawing.Point(95, 163);
+            this.logInBtn.Location = new System.Drawing.Point(127, 201);
+            this.logInBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.logInBtn.Name = "logInBtn";
-            this.logInBtn.Size = new System.Drawing.Size(75, 23);
+            this.logInBtn.Size = new System.Drawing.Size(100, 28);
             this.logInBtn.TabIndex = 0;
             this.logInBtn.Text = "Log In";
             this.logInBtn.UseVisualStyleBackColor = true;
@@ -53,33 +54,37 @@ namespace Institute_system
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 49);
+            this.label1.Location = new System.Drawing.Point(60, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.Size = new System.Drawing.Size(73, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "userName";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 93);
+            this.label2.Location = new System.Drawing.Point(60, 114);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.Size = new System.Drawing.Size(68, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "password";
             // 
             // userNameTextBox
             // 
-            this.userNameTextBox.Location = new System.Drawing.Point(118, 46);
+            this.userNameTextBox.Location = new System.Drawing.Point(157, 57);
+            this.userNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.userNameTextBox.Name = "userNameTextBox";
-            this.userNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userNameTextBox.Size = new System.Drawing.Size(132, 22);
             this.userNameTextBox.TabIndex = 3;
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(118, 90);
+            this.passwordTextBox.Location = new System.Drawing.Point(157, 111);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.Size = new System.Drawing.Size(132, 22);
             this.passwordTextBox.TabIndex = 4;
             // 
             // loginTypeComboBox
@@ -88,47 +93,51 @@ namespace Institute_system
             this.loginTypeComboBox.Items.AddRange(new object[] {
             "student",
             "instructor"});
-            this.loginTypeComboBox.Location = new System.Drawing.Point(79, 136);
+            this.loginTypeComboBox.Location = new System.Drawing.Point(105, 167);
+            this.loginTypeComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.loginTypeComboBox.Name = "loginTypeComboBox";
-            this.loginTypeComboBox.Size = new System.Drawing.Size(108, 21);
+            this.loginTypeComboBox.Size = new System.Drawing.Size(143, 24);
             this.loginTypeComboBox.TabIndex = 5;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.ImageLocation = "C:\\Users\\Blu-Ray\\OneDrive\\Desktop\\iti\\Sql server Advanced\\Project\\iti-logo.png";
-            this.pictureBox1.Location = new System.Drawing.Point(235, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(35, 36);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(226, 134);
+            this.button1.Location = new System.Drawing.Point(301, 165);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 7;
             this.button1.Text = "update student";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // Logo
+            // 
+            this.Logo.Image = global::Institute_system.Properties.Resources.iti_logo;
+            this.Logo.Location = new System.Drawing.Point(301, 2);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(105, 145);
+            this.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Logo.TabIndex = 8;
+            this.Logo.TabStop = false;
+            this.Logo.WaitOnLoad = true;
+            // 
             // login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(326, 217);
+            this.ClientSize = new System.Drawing.Size(435, 267);
+            this.Controls.Add(this.Logo);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.loginTypeComboBox);
             this.Controls.Add(this.passwordTextBox);
             this.Controls.Add(this.userNameTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.logInBtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "login";
             this.Text = "login";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,8 +151,8 @@ namespace Institute_system
         private System.Windows.Forms.TextBox userNameTextBox;
         private System.Windows.Forms.TextBox passwordTextBox;
         private System.Windows.Forms.ComboBox loginTypeComboBox;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox Logo;
     }
 }
 
