0cd890c771d0e9f6e76f123e5e3de21d34ebb860
hagermontaser5 <hagermontaser5@gmail.com>
2022-03-21 23:32:07 +0200

    Implemented Business Reports

--- Institute_system/itiDBmodel.Designer.cs
+++ Institute_system/itiDBmodel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Blu-Ray\OneDrive\Desktop\iti\Sql server Advanced\instituteExaminationSystem\Institute_system\itiDBmodel.edmx'. 
+﻿// T4 code generation is enabled for model 'H:\ITI\subjects\Advanced SQL\project\VS\Institute_system\itiDBmodel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
